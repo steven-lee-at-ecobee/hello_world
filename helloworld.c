0aa6ba19c957d7f674222fe5e52cc5fbd850c164
Steven Lee <steven.lee@ecobee.com>
2023-07-17 11:02:39 -0400

    Create factorial function

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+unsigned int factorial(unsigned int number)
+{
+    return number <= 1 ? 1 : factorial(number - 1) * number;
+}
+
 int main() {
     printf("Hello, World!\n");
     return 0;
